1a03907176e1c0d5e8787d357f9db805af64bb1b
Christian Buehlmann <bue@foryouandyourcustomers.com>
2021-01-22 16:11:29 +0100

    publish basic entity and attributes

--- notebooks/confluence-export/Publisher.py
+++ notebooks/confluence-export/Publisher.py
@@ -1,11 +1,16 @@
 from xml.sax.saxutils import escape
 from datetime import datetime
+import logging
+
+
 
 class Publisher:
     '''
     The publisher contains mapping information of IM elements.
     And helper methods to translate content from IM json to Confluence
     '''
+
+    log = logging.getLogger(__name__)
 
     def __init__(self, config, data, confluence, space_key, root_page_id, language='de'):
         self.config = config
@@ -28,30 +33,38 @@ class Publisher:
         '''Returns the url of an element'''
         return str(element_key)
 
-    def page(self, element_key):
-        '''Returns the page object of an element or None if there is no page yet
-        A page object is a dictionary containing 'pageid' and 'name'
-        '''
-        return pagemap.get(element_key)
-
-    def register_page_id(self, element_key, page_id):
-        element = content_map.get(element_key, {})
-        element['pageid'] = page_id
-
-
     def scan_current_content(self):
         '''Scan current content below page-root and fills the content_map accordingly'''
         None   # Nothing found
 
+    def register_page(self, key: str, page_id: str, title: str):
+        element = self.content_map.get(key)
+        if element:
+            self.log.warning('Element {} will be overwritten'.format(key))
+        element = {}
+        element['pageid'] = page_id
+        element['title'] = title
+        self.content_map[key] = element
+
+    def page_for_key(self, key: str):
+        '''Returns the page object of an element or None if there is no page yet
+        A page object is a dictionary containing 'pageid' and 'name'
+        '''
+        return self.content_map.get(key)
 
     def stub(self, title: str, parent_page_id):
-        '''Create a stub page to obtain the page id for a specific title'''
+        '''Create a stub page to obtain the page id for the title'''
         if self.confluence.page_exists(self.space_key, title):
             key = self.confluence.get_page_id(self.space_key, title)
             if key:
                 return { 'id': str(key) }
             assert false, 'illegal condition, page exists but unable to retreive the id'
-        return self.confluence.create_page(self.space_key, title=title, parent_id=parent_page_id, body=(''))
+        create_result = self.confluence.create_page(self.space_key, title=title, parent_id=parent_page_id, body=('generated stub'))
+        return create_result
+
+    def update_page(self, key: str, body: str):
+        meta = self.page_for_key(key)
+        self.confluence.update_page(meta['pageid'], meta['title'], body)
 
     def publish_attributes(self):
         None
