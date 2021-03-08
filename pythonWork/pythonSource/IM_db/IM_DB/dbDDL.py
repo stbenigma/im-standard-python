9b7b68d4e3ad9372e609c748960ee77e544895aa
stb@foryouandyourcustomers.com <stb@foryouandyourcustomers.com>
2021-03-08 08:13:06 +0100

    dbDx merged from fix-id

--- pythonWork/pythonSource/IM_db/IM_DB/dbDDL.py
+++ pythonWork/pythonSource/IM_db/IM_DB/dbDDL.py
@@ -4,61 +4,98 @@ from IM_DB import *
 import sqlite3
 import re
 
-#demo_table = """
-#CREATE TABLE temp(
-#id INTEGER PRIMARY KEY NOT NULL,
-#fname VARCHAR(20),
-#joining DATE NOT NULL,
-#lohn NUMBER(8,2));"""
-
 def createTable(psql):
     cursor = dbConnect.myDbConn.cursor()
 
     try:
         cursor.execute(psql)
     except sqlite3.Error as e:
-        if re.match("table .* already exists",e.__str__()):
+        if re.match("table .* already exists", e.__str__()):
             pass
         else:
-            print ("Unerwarteter SQL-Fehler: \t{}" .format(e))
+            print("Unerwarteter SQL-Fehler: \t{}".format(e))
             raise e
-#end createTable
+    return
+
+
+# end createTable
 
 def dropTable(ptableName):
     cursor = dbConnect.myDbConn.cursor()
 
     try:
-        cursor.execute("drop table "+ptableName+";")
+        cursor.execute("drop table " + ptableName + ";")
     except sqlite3.Error as e:
-        if re.match("no such table:.* ",e.__str__()):
+        if re.match("no such table:.* ", e.__str__()):
             pass
         else:
-            print ("Unerwarteter SQL-Fehler: \t{}" .format(e))
+            print("Unerwarteter SQL-Fehler: \t{}".format(e))
             raise e
+    return
 #end dropTable
 def dropView(ptableName):
     cursor = dbConnect.myDbConn.cursor()
 
     try:
-        cursor.execute("drop view "+ptableName+";")
+        cursor.execute("drop view " + ptableName + ";")
     except sqlite3.Error as e:
-        if re.match("no such view:.* ",e.__str__()):
+        if re.match("no such view:.* ", e.__str__()):
             pass
         else:
-            print ("Unerwarteter SQL-Fehler: \t{}" .format(e))
+            print("Unerwarteter SQL-Fehler: \t{}".format(e))
             raise e
-#end dropView
+    return
+
+# end dropView
 
 def execscript(psql):
     cursor = dbConnect.myDbConn.cursor()
     try:
         cursor.executescript(psql)
     except sqlite3.Error as e:
-        if re.match("xxxxxxx",e.__str__()):
+        if re.match("xxxxxxx", e.__str__()):
             pass
         else:
             print(psql)
-            print ("exec: unexpected SQL-error: \t%s" % e)
+            print("exec: unexpected SQL-error: \t%s" % e)
             raise e
     dbConnect.myDbConn.commit()
-#end executescript
+# end executescript
+
+def getsoleukcolname(ptablename):
+    uks = getuklist(ptablename)
+    if len(uks)== 1 and len(uks[0])==1:
+        return uks[0][0]
+    else:
+        return None
+
+def getuklist(ptablename):
+    cursor = dbConnect.myDbConn.cursor()
+    cursor.execute("PRAGMA index_list('{}')".format(ptablename))
+    indices = cursor.fetchall()
+    """index_list [seq,name,unique(0,1),origin (u),partial]
+        index_info [seqno,cid,colname]
+        foreign_key_list [id,seq,table,from,to,on_update (NO_ACTION),on_delete(CASCADE),match]
+        """
+    """List of all uk (list of columns) for this table  [[colname,],]"""
+    retval = []
+    for i in indices:
+        if i[2] == 1 and i[3] == 'u':
+            """unique key"""
+            cursor.execute("PRAGMA index_info('{}')".format(i[1]))
+            cols = cursor.fetchall()
+            retval.append([c[2] for c in sorted(cols)])
+        #fi
+    #for
+    cursor.close()
+    return retval
+
+def getfklist(ptablename):
+    cursor = dbConnect.myDbConn.cursor()
+    cursor.execute("PRAGMA foreign_key_list('{}')".format(ptablename))
+    fks = cursor.fetchall()
+    """foreign_key_list [id,seq,table,from,to,on_update (NO_ACTION),on_delete(CASCADE),match]
+       return {colname:(fktable,fkcolname)}"""
+    retval = {fk[3]: (fk[2], fk[4]) for fk in fks}
+    cursor.close()
+    return retval
--- pythonWork/pythonSource/IM_db/IM_DB/dbDML.py
+++ pythonWork/pythonSource/IM_db/IM_DB/dbDML.py
@@ -1,8 +1,10 @@
 # -*- coding: latin-1 -*-
 
-from IM_DB import logmessages,dbConnect
-import sqlite3
 import re
+import sqlite3
+
+from IM_DB import logmessages, dbConnect
+
 
 def select(psql):
     cursor = dbConnect.myDbConn.cursor()
@@ -10,15 +12,17 @@ def select(psql):
     try:
         cursor.execute(psql)
     except sqlite3.Error as e:
-        if re.match("table .* already exists",e.__str__()):
+        if re.match("table .* already exists", e.__str__()):
             pass
         else:
-            print (psql)
-            print ("select: Unerwarteter SQL-Fehler: \t%s" % e)
+            print(psql)
+            print("select: Unerwarteter SQL-Fehler: \t%s" % e)
             raise e
     result = cursor.fetchall()
     return result
-#end select
+
+
+# end select
 
 def lookup(psql):
     cursor = dbConnect.myDbConn.cursor()
@@ -26,35 +30,37 @@ def lookup(psql):
     try:
         cursor.execute(psql)
     except sqlite3.Error as e:
-        if re.match("bekannter Fehler",e.__str__()):
+        if re.match("bekannter Fehler", e.__str__()):
             pass
         else:
-            print ("lookup: Unerwarteter SQL-Fehler: \t%s" % e)
+            print("lookup: Unerwarteter SQL-Fehler: \t%s" % e)
             raise e
     result = cursor.fetchall()
     if result == []:
         e = sqlite3.DataError('No Data Found')
         raise e
-    #print ('result=',result)
+    # print ('result=',result)
     return result[0][0]
-#lookup
 
-def delete(ptableName,pwhere=None):
-    mydb=dbConnect.myDbConn
+
+# lookup
+
+def delete(ptableName, pwhere=None):
     cursor = dbConnect.myDbConn.cursor()
     try:
-        cursor.execute("delete from {} where {}".format(ptableName,"1=1" if pwhere is None else pwhere))
+        sql = "delete from {} where {}".format(ptableName, "1=1" if pwhere is None else pwhere)
+        cursor.execute(sql)
     except sqlite3.Error as e:
-        if re.match("table .* already exists",e.__str__()):
-            pass
-        else:
-            logmessages.writelog("delete: unexpected SQL-error: \t%s" % e)
-            raise e
+        logmessages.writelog(sql)
+        logmessages.writelog("unexpected SQL-error: \t%s" % e)
+        raise e
     dbConnect.myDbConn.commit()
-#delete
 
-def insert(psql,rec):
-    #print (psql,rec)
+
+# delete
+
+def insert(psql, rec):
+    # print (psql,rec)
     cursor = dbConnect.myDbConn.cursor()
 
     try:
@@ -65,57 +71,88 @@ def insert(psql,rec):
         else:
             raise Exception("unknown type for insert {}".format(type(rec)))
     except sqlite3.IntegrityError as ei:
-        #Unique kann für Indexweiterzählen gebraucht werden. darum keine Fehlermeldung
+        # Unique kann für Indexweiterzählen gebraucht werden. darum keine Fehlermeldung
         if not str(ei).startswith('UNIQUE'):
             logmessages.writelog(psql)
             logmessages.writelog(rec)
             logmessages.writelog(type(rec))
-            logmessages.writelog("insert: Constraint-error: \t{}" .format (str(ei)))
-        #fi
+            logmessages.writelog("insert: Constraint-error: \t{}".format(str(ei)))
+        # fi
         raise ei
     except sqlite3.Error as e:
         logmessages.writelog(psql)
         logmessages.writelog(rec)
         logmessages.writelog(type(rec))
-        logmessages.writelog ("insert: unexpected SQL-error: \t{}" .format (str(e)))
+        logmessages.writelog("insert: unexpected SQL-error: \t{}".format(str(e)))
         raise e
     id = cursor.lastrowid
     dbConnect.myDbConn.commit()
     return id
-#insert
 
-def insertmany(psql,rec):
-    insert(psql,rec)
+
+# insert
+
+def insertmany(psql, rec):
+    insert(psql, rec)
     return
-#end insertmany
 
-def exec(psql,*args):
-    #print (psql)
-    #return
+# end insertmany
+
+def exec(psql, *args):
+    # print (psql)
+    # return
     cursor = dbConnect.myDbConn.cursor()
     try:
-        cursor.execute(psql,args)
+        cursor.execute(psql, args)
     except sqlite3.Error as e:
-        if re.match("xxxxxxx",e.__str__()):
+        if re.match("xxxxxxx", e.__str__()):
             pass
         else:
-            logmessages.writelog (psql)
-            logmessages.writelog ("exec: unexpected SQL-error: \t%s" % e)
+            logmessages.writelog(psql)
+            logmessages.writelog("exec: unexpected SQL-error: \t%s" % e)
             raise e
     dbConnect.myDbConn.commit()
-#end exec
-def execmany(psql,recs):
-    #print (psql)
-    #return
+
+
+# end exec
+def execmany(psql, recs):
+    # print (psql)
+    # return
     cursor = dbConnect.myDbConn.cursor()
     try:
-        cursor.executemany(psql,recs)
+        cursor.executemany(psql, recs)
     except sqlite3.Error as e:
-        if re.match("xxxxxxx",e.__str__()):
+        if re.match("xxxxxxx", e.__str__()):
             pass
         else:
-            logmessages.writelog (psql)
-            logmessages.writelog ("execmany: unexpected SQL-error: \t%s" % e)
+            logmessages.writelog(psql)
+            logmessages.writelog("execmany: unexpected SQL-error: \t%s" % e)
             raise e
     dbConnect.myDbConn.commit()
-#execmany
+
+
+# execmany
+
+def valuepairs2sqlexpr(**colvalues):
+    """input: {colname:colvalue,}
+       return "(col-name is NULL or col-name = value)" and concatenated for every colname/-value pair
+       if value is not of integer type, enclose it with '' """
+    sqlstring = lambda val: "'{}'".format(val) if type(val) != int else str(val)
+    comp = lambda col, val: "{} is null".format(col) if val is None else "{} = {}".format(col, sqlstring(val))
+    retval = " and ".join("({})".format(comp(col, val)) for col, val in colvalues.items())
+    return retval
+
+
+def id2uktranslate():
+    """from currently open db, return list of all id's together with their UK-columns
+        {<tableshortname>id : {colname:value,}} for every column being in a uk
+    """
+    tables = select(psql="""SELECT name
+                            FROM sqlite_master
+                            WHERE type = 'table' 
+                            AND name NOT LIKE'sqlite_%'""")
+    retval = {}
+    for tab in tables:
+        t = tab[0]
+        rows = t
+
