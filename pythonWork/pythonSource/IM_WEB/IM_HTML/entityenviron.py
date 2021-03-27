9b82fb6171ec9a03655012fb6e0cbba845910242
stb@foryouandyourcustomers.com <stb@foryouandyourcustomers.com>
2021-03-27 14:22:18 +0100

    svg generieriung begonnen

--- pythonWork/pythonSource/IM_WEB/IM_HTML/entityenviron.py
+++ pythonWork/pythonSource/IM_WEB/IM_HTML/entityenviron.py
@@ -4,16 +4,29 @@ from IM_JSON import JSModel
 nvl = lambda str: str if str is not None else ''
 
 class EntityCell():
-    def __init__(self,pentiid=None,pentiname=None,passoc=None):
+    CENTER = 'center'
+    ROLE = 'role'
+    SUPER = 'super'
+    PARENT = 'parent'
+    CHILD = 'child'
+
+    def __init__(self,ptype=None,pentiid=None,pentiname=None,passoc=None):
         self.setentiid(pentiid)
         self.setentiname(pentiname)
         self.setassoc(passoc)
+        self.settype(nvl(ptype))
 
     def getentiid(self):
         return self._entiid
 
     def setentiid(self,pentiid):
         self._entiid = pentiid
+
+    def gettype(self):
+        return self._type
+
+    def settype(self,ptype):
+        self._type = ptype
 
     def getentiname(self):
         return self._entiname
@@ -26,74 +39,66 @@ class EntityCell():
     def setassoc(self,passoc):
         self._assoc = passoc
 
+    def isemptycell(self):
+        return self.gettype() == ''
+
     def __str__(self):
-        return ', '.join ((nvl(self.getentiid()),nvl(self.getentiname()),nvl(self.getassoc())))
+        return ', '.join ((self.gettype(), nvl(self.getentiid()), nvl(self.getentiname()), nvl(self.getassoc())))
 
 
 class EntityEnvironment():
-    ROLE = 'role'
-    SUPER = 'super'
-    PARENT = 'parent'
-    CHILD = 'child'
-
     """Dictrionary for one entity.
-        two-dimensional etensible grid 1. index (hidx)  -> left to right, 2. index (vidx) top down
+        two-dimensional extensible grid 1. index (vidx)  top down , 2. index (hidx) -> left to right
         0/0 is the center
         1,2,3 are cells to downwards or to the right
         -1,-2,-3 are cells upwards or to the left
         {  ...
-          ,-1 : {}   HIDX
-          ,0 : {...
-                ,-1 :  VIDX
-                ,0 : [entiid,entiname,relationtext]
-                ,1 :
-                ...}
+          ,-1 : {}   vidx
+          ,0 : {'left'
+                'center':[entiid,entiname,relationtext]
+                right :
+                }
           ,1 : {}
           ,...}
     """
     def __init__(self,pentiid,pentiname):
         self._grid = dict()
-        self.fillcell (phidx=0,pvidx=0,pcell=EntityCell(pentiid=pentiid,pentiname=pentiname))
+        self.fillcell (pvidx=0,phidx='center',pcell=EntityCell(ptype=EntityCell.CENTER,pentiid=pentiid,pentiname=pentiname))
         return
 
-    def fillcell(self,phidx,pvidx,pcell:EntityCell):
-        if phidx not in self._grid.keys():
-            self._grid[phidx] = {}
-        self._grid[phidx][pvidx] = pcell
+    def fillcell(self,pvidx,phidx,pcell:EntityCell):
+        if pvidx not in self._grid.keys():
+            self._grid[pvidx] = {}
+        self._grid[pvidx][phidx] = pcell
 
-    def getcell (self,phidx,pvidx):
-        if phidx not in self._grid.keys():
+    def getcell (self,pvidx,phidx):
+        if pvidx not in self._grid.keys():
             return EntityCell() #non existing cell is empty
-        if pvidx not in  self._grid[phidx].keys():
+        if phidx not in  self._grid[pvidx].keys():
             return EntityCell()
-        return self._grid[phidx][pvidx]
-
-    def getminhkey(self):
-        return min(self._grid.keys())
-    def getmaxhkey(self):
-        return max(self._grid.keys())
+        return self._grid[pvidx][phidx]
 
     def getminvkey(self):
-        return min(min(vkey for vkey in cols.keys()) for cols in self._grid.values())
+        return min(self._grid.keys())
     def getmaxvkey(self):
-        return max(max(vkey for vkey in cols.keys()) for cols in self._grid.values())
+        return max(self._grid.keys())
 
-    def getentiid(self, phidx, pvidx):
-        cell = self.getcell(phidx=phidx, pvidx=pvidx)
+    def getentiid(self, pvidx, phidx):
+        cell = self.getcell(pvidx=pvidx, phidx=phidx)
         if len(cell) == 0:
             return None
         else:
             return cell.getentiid()
 
-    def getentiname(self, phidx, pvidx):
-        cell = self.getcell(phidx=phidx, pvidx=pvidx)
+    def getentiname(self, pvidx, phidx):
+        cell = self.getcell(pvidx=pvidx, phidx=phidx)
         if len(cell) == 0:
             return None
         else:
             return cell.getentiname()
 
-    def getassoc(self, phidx, pvidx):
-        cell = self.getcell(phidx=phidx, pvidx=pvidx)
+    def getassoc(self, pvidx, phidx):
+        cell = self.getcell(pvidx=pvidx, phidx=phidx)
         if len(cell) == 0:
             return None
         else:
@@ -101,27 +106,25 @@ class EntityEnvironment():
 
     def togrid(self, pcells: list, ptype):
         #doubleline = len(pcells) > 2
-        if ptype == EntityEnvironment.ROLE:
-            hidx, vidx = 1, 0
-        elif ptype == EntityEnvironment.SUPER:
-            hidx, vidx = -1, 0
-        elif ptype == EntityEnvironment.PARENT:
-            hidx, vidx = 0, -1
-        elif ptype == EntityEnvironment.CHILD:
-            hidx, vidx = 0, 1
+        if ptype == EntityCell.ROLE:
+            vidx, hidx = 0, 'right'
+        elif ptype == EntityCell.SUPER:
+            vidx, hidx = 0, 'left'
+        elif ptype == EntityCell.PARENT:
+            vidx, hidx = -1, 'center'
+        elif ptype == EntityCell.CHILD:
+            vidx, hidx = 1, 'center'
+        elif ptype == EntityCell.CENTER:
+            vidx, hidx = 0, 'center'
         else:
             assert false, "illegal type '{}'".format(ptype)
 
         for cell in pcells:
-            self.fillcell(pvidx=hidx, phidx=vidx,pcell=cell)
-            #hidx = switchhidx(hidx, doubleline) moved to display procedure
-            vidx += 1 if ptype in (EntityEnvironment.CHILD, EntityEnvironment.SUPER) else -1
+            self.fillcell(pvidx=vidx, phidx=hidx,pcell=cell)
+            #vidx = switchvidx(vidx, doubleline) moved to display procedure
+            vidx += 1 if ptype in (EntityCell.SUPER, EntityCell.CHILD) else -1
         # for
         return
-
-"""switch to second line for next entity"""
-sign = lambda x: (1, -1)[x<0]
-switchhidx = lambda hidx,doubleline:  1*sign(hidx) if ((abs(hidx) == 2) or not doubleline) else  2*sign(hidx)
 
 def related(pentiid,prelated,pjson,pcardinality,pmodellang):
     retval = []
@@ -140,7 +143,8 @@ def related(pentiid,prelated,pjson,pcardinality,pmodellang):
         else:
             continue # not my relation or I am not a child or I am recursive
         #fi
-        retval.append(EntityCell(pentiid=parentid, pentiname=entities[parentid]['name'][pmodellang],passoc=assoc))
+        retval.append(EntityCell(ptype=EntityCell.PARENT if pcardinality == Relation.ONE else EntityCell.CHILD
+                                 ,pentiid=parentid, pentiname=entities[parentid]['name'][pmodellang],passoc=assoc))
     #for
     return retval
 
@@ -152,21 +156,51 @@ def createentienvironment(pentiid,pjson:JSModel,pmodellang):
     if not pentiid in entities.keys(): return None #non existing entity is Nothing
 
     entienvir = EntityEnvironment(pentiid=pentiid,pentiname=entities[pentiid]['name'][pmodellang])
-    enties = [EntityCell(pentiid=entiid, pentiname=entities[entiid]['name'][pmodellang]) for entiid in entities[pentiid]['roles+'] + entities[pentiid]['subtypes+']]
-    entienvir.togrid(pcells=enties, ptype=EntityEnvironment.ROLE)
+    enties = [EntityCell(ptype=EntityCell.ROLE,pentiid=entiid, pentiname=entities[entiid]['name'][pmodellang]) for entiid in entities[pentiid]['roles+'] + entities[pentiid]['subtypes+']]
+    entienvir.togrid(pcells=enties, ptype=EntityCell.ROLE)
 
-    enties = [EntityCell(pentiid=entiid, pentiname=entities[entiid]['name'][pmodellang]) for entiid in entities[pentiid]['supertypes+']]
-    entienvir.togrid(pcells=enties, ptype=EntityEnvironment.SUPER)
+    enties = [EntityCell(ptype=EntityCell.SUPER,pentiid=entiid, pentiname=entities[entiid]['name'][pmodellang]) for entiid in entities[pentiid]['supertypes+']]
+    entienvir.togrid(pcells=enties, ptype=EntityCell.SUPER)
 
     """handle Parents (I am ONE, parent is MANY)"""
     parents = related(pentiid=pentiid,prelated=entities[pentiid]['relations+'], pjson=pjson, pcardinality=Relation.ONE, pmodellang=pmodellang)
-    entienvir.togrid(pcells=parents, ptype=EntityEnvironment.PARENT)
+    entienvir.togrid(pcells=parents, ptype=EntityCell.PARENT)
 
     """handle children (I am MANY, Child is ONE or MANY)"""
     children = related(pentiid=pentiid,prelated=entities[pentiid]['relations+'], pjson=pjson, pcardinality=Relation.MANY, pmodellang=pmodellang)
-    entienvir.togrid(pcells=children, ptype=EntityEnvironment.CHILD)
+    entienvir.togrid(pcells=children, ptype=EntityCell.CHILD)
 
     return entienvir
+
+def enti2svg():
+    entistart = """
+    <g  fill="{}" stroke="{}" fill-opacity="{}" stroke-opacity="{}" 
+            transform="translate({},{})" >
+        <rect x="0" y="0" width="{}" height="{}" rx="10" ry="10" /><a href="#{}" >
+        <text id="{}" x="20" y="13" fill="{}" font-weight="bold"  fill-opacity="1.0" font-size="{}" stroke="none">
+        {} </text></a>
+    """
+    entiende = """</g>"""
+
+    entisvg = entistart
+    entisvg += entiende
+    return entisvg
+
+def entienviro2svg(penviron):
+    diagramhead ="""
+    <div id="{}-container">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                version="1.1"  width="{}" height="{}">
+        <defs id="dmw_defs" >
+        </defs>
+    """
+    diagramfoot ="""
+        </svg>
+    </div>
+    """
+    svgtext = diagramhead
+    svgtext += diagramfoot
+    return svgtext
 
 
 from IM_OBJECTS import Modelelemtype,Relation
--- pythonWork/pythonSource/IM_WEB/testentienviron.py
+++ pythonWork/pythonSource/IM_WEB/testentienviron.py
@@ -10,16 +10,47 @@ def main(pjson, plang):
     modellang = jsmodel.getelements(pelemtype='PROJ')["language"] if plang is None else plang
 
     for idx, entiid in enumerate(jsmodel.getelements(pelemtype=Modelelemtype.ENTI).keys()):
-        if entiid not in ("ENTI315","ENTI313","ENTI316","ENTI318","ENTI307","ENTI335","ENTI329"): continue
+        #if entiid not in ("ENTI315","ENTI313","ENTI316","ENTI318","ENTI307","ENTI335","ENTI329"): continue
         enti =jsmodel.getelements(pelemtype=Modelelemtype.ENTI)[entiid]
-        print (entiid,enti['name'][plang],enti["roles+"],enti['subtypes+'],enti["supertypes+"],enti["relations+"])
         entienvir = entityenviron.createentienvironment(pentiid=entiid,pjson=jsmodel,pmodellang=modellang)
-        for hkey in range(entienvir.getminhkey(),entienvir.getmaxhkey()+1):
-            line = str(hkey).ljust(4)+': '
-            for vkey in range(-1,2):
-                line += ' | '+entityenviron.nvl(entienvir.getcell(phidx=hkey,pvidx=vkey).getentiname()).ljust(20)[:20]
-            print (line)
+        #print (entiid,enti['name'][plang],enti["roles+"],enti['subtypes+'],enti["supertypes+"],enti["relations+"],entienvir.getminvkey(),entienvir.getmaxvkey())
+        for vkey in range(entienvir.getminvkey(),entienvir.getmaxvkey()+1):
+            line = str(vkey).ljust(4)+': '
+            cellleft = entienvir.getcell(phidx='left', pvidx=vkey)
+            cellcenter = entienvir.getcell(phidx='center', pvidx=vkey)
+            cellright = entienvir.getcell(phidx='right', pvidx=vkey)
+            #print ('**',vkey,cellleft,cellcenter,cellright,line)
+
+            if cellleft.gettype() == entityenviron.EntityCell.SUPER:
+                line += ' | '+entityenviron.nvl(cellleft.getentiname()).ljust(20)[:20]
+            elif cellcenter.gettype() == entityenviron.EntityCell.PARENT:
+                line += ' | ' + entityenviron.nvl(cellcenter.getentiname()).ljust(20)[:20]
+            else:
+                line += ' | ' + ' '.ljust(20)
+            #fi
+            if cellcenter.gettype() == entityenviron.EntityCell.CENTER:
+                line += ' | '+entityenviron.nvl(cellcenter.getentiname()).ljust(20)[:20]
+            elif cellcenter.gettype() == entityenviron.EntityCell.PARENT:
+                line += ' | ' + entityenviron.nvl(cellcenter.getassoc()).ljust(20)[:20]
+            elif cellcenter.gettype() == entityenviron.EntityCell.CHILD:
+                line += ' | ' + entityenviron.nvl(cellcenter.getassoc()).ljust(20)[:20]
+            else:
+                line += ' | ' + ' '.ljust(20)
+            #fi
+            if cellright.gettype() == entityenviron.EntityCell.ROLE:
+                line += ' | '+entityenviron.nvl(cellright.getentiname()).ljust(20)[:20]
+            elif cellcenter.gettype() == entityenviron.EntityCell.CHILD:
+                line += ' | ' + entityenviron.nvl(cellcenter.getentiname()).ljust(20)[:20]
+            else:
+                line += ' | ' + ' '.ljust(20)
+            #fi
+            print(line)
+        #for
         #if idx > 10: break
+        print()
+        print (entityenviron.entienviro2svg(penviron = entienvir))
+    #for
+
 # main
 
 
