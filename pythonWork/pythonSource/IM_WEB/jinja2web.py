eab45ff2296c939d25b9625956ee18fd1cb444ef
stb@foryouandyourcustomers.com <stb@foryouandyourcustomers.com>
2021-05-29 15:47:16 +0200

    Markdown template fertiggestellt
    Signed-off-by: stb@foryouandyourcustomers.com <stb@foryouandyourcustomers.com>

--- pythonWork/pythonSource/IM_WEB/jinja2web.py
+++ pythonWork/pythonSource/IM_WEB/jinja2web.py
@@ -185,9 +185,9 @@ def formattext(pstr:str):
     #check wether we have markdown in the string
     if type(pstr) != str:
         return pstr
-    if pstr.startswith(texttype[0]):
+    if pstr.startswith(texttypes[0]):
         try:
-            htmltext = markdown(pstr[len(texttype[0]):])
+            htmltext = markdown(pstr[len(texttypes[0]):])
             """ mark html tags with a special class to allow css for markdown content"""
             htmltext = re.sub(r'<(h1|h2|h3|h4|p|li|ul|ol)>', '<\g<1> class="md">',htmltext)
             return htmltext
