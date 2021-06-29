2ee2432b23eec6351254b5d8a01a5b9dc931773b
stb@foryouandyourcustomers.com <stb@foryouandyourcustomers.com>
2021-06-29 18:45:38 +0200

    Map Excel

--- pythonWork/pythonSource/tools/mapping/createMapXLS.py
+++ pythonWork/pythonSource/tools/mapping/createMapXLS.py
@@ -8,43 +8,31 @@ import sys,os
 from openpyxl import Workbook
 
 val2str = lambda v: '' if v is None else str(v) if type(v) in (int,float) else v
-def writesheets(pwb):
-    tables = []
-    tables = [tab.upper() for tab in tables]
-    tables = sorted(tables)
+def writesheets(pwb,pjsmodel:JSModel):
+    systems = pjsmodel.getelements('INTF')
     ws = pwb.create_sheet("Overview")
     rowidx, colidx = 1, 1
-    ws.cell(column=colidx, row=rowidx, value='Table')
-    ws.cell(column=colidx+1, row=rowidx, value='rowcount')
-    return
-    for tab in tables:
+    ws.cell(column=colidx, row=rowidx, value='System')
+    ws.cell(column=colidx+1, row=rowidx, value='tablecount')
+    for sys in systems.values():
         rowidx += 1
-        ws.cell(column=colidx, row=rowidx, value=tab)
-        ws.cell(column=colidx+1, row=rowidx, value=dbDML.getrowcount(ptablename=tab))
+        ws.cell(column=colidx, row=rowidx, value=sys['name'])
+        ws.cell(column=colidx+1, row=rowidx, value=len(sys["tables+"]))
     #for
-    for tab in tables:
-        ws = pwb.create_sheet(tab)
-        cols = dbDDL.getcolums(ptablename=tab)
-        rowidx, colidx = 1, 2
-        #headers, starting at 2, leaving room for CRUD column for importing
-        for col in cols.keys():
-            ws.cell(column=colidx, row=rowidx, value=col)
-            colidx += 1
-        #for
-        data = dbDML.select(psql="select * from {}".format(tab))
-        for row in data:
+    for sys in systems.values():
+        ws = pwb.create_sheet(sys["name"])
+        tables = [pjsmodel.getbyid(tab) for tab in sys["tables+"]]
+        tables = sorted(tables,key=lambda tab:tab["name"])
+        rowidx, colidx = 1, 1
+        for tab in tables:
+            ws.cell(column=colidx, row=rowidx, value=tab["name"])
             rowidx += 1
-            colidx = 2
-            for v in row:
-                ws.cell(column=colidx, row=rowidx, value=val2str(v))
-                colidx +=1
-            #for
-        # for
+        #for
     return
 
-def createExcel(pfilename: str):
+def createExcel(pfilename: str,pjsmodel):
     wb = Workbook()
-    writesheets(pwb=wb)
+    writesheets(pwb=wb,pjsmodel=pjsmodel)
     wb.remove(wb.worksheets[0])
     wb.save(filename=pfilename)
     return
@@ -54,7 +42,8 @@ def main(param1):
     jsmodel = JSModel.readfromfile(pfilename=param1)
     filename = param1[:-len(JSONEXTENSION)]+'_MAP'
     fileext = '.xlsx'
-    createExcel(pfilename=filename+fileext)
+    createExcel(pfilename=filename+fileext,pjsmodel=jsmodel)
+    print ("Excel {} created".format (filename+fileext))
 
 if __name__ == '__main__':
     main(param1=None if len(sys.argv) == 1 else sys.argv[1])
