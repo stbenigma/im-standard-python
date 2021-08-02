7e6f76a0a75ae53401ba800e0298241ca4f97568
stb@foryouandyourcustomers.com <stb@foryouandyourcustomers.com>
2021-08-02 17:06:58 +0200

    Linesegements in json dict->list

--- pythonWork/pythonSource/IM_db/IM_OBJECTS/representation.py
+++ pythonWork/pythonSource/IM_db/IM_OBJECTS/representation.py
@@ -72,7 +72,7 @@ class Relationrep(Baseobject):
 
 
     def getlinesegments(self):
-        return Linesegment.select(pwhere=("lise_relr_id = ?", self.relr_id))
+        return Linesegment.select(pwhere=("lise_relr_id = ?", self.relr_id),porderby="lise_seq")
 
 
 # relationrep
