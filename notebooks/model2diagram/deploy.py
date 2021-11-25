3892abd353264b7da7744a7f3bd4c0691f30c72c
Christian Buehlmann <bue@foryouandyourcustomers.com>
2021-11-25 06:27:27 +0100

    add zip builder

--- notebooks/model2diagram/deploy.py
+++ notebooks/model2diagram/deploy.py
@@ -3,9 +3,12 @@ import os
 from nbconvert import PythonExporter
 from traitlets.config import Config
 import nbformat as nbf
-from nbconvert.preprocessors import TagRemovePreprocessor
+import logging
+import zipfile
+
 
 script_file = 'generator.ipynb'
+
 
 def notebook_to_python(source, exporter, destination):
     with open(source, 'r') as src:
@@ -26,11 +29,33 @@ c.PythonExporter.preprocessors = ["nbconvert.preprocessors.TagRemovePreprocessor
 
 exporter = PythonExporter(config=c)
 
-
 filename = os.path.basename(script_file)
 noext, _ = os.path.splitext(filename)
-
 target = noext + '.py'
-notebook_to_python(script_file, exporter, target)
 
-print(f"Generated {target} from {script_file}")
+logging.info(f"Converting {script_file} to plain python")
+notebook_to_python(script_file, exporter, target)
+logging.info(f"Generated {target} from {script_file}")
+
+
+
+
+def zipdir(path, ziph, content_root):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            path = os.path.join(root, file)
+            ziph.write(path, os.path.relpath(path, content_root))
+
+
+package_name = 'model2diagram'
+
+archive = os.path.join('.', package_name + '.zip')
+
+with zipfile.ZipFile(archive, 'w', compression=zipfile.ZIP_DEFLATED) as zipfile:
+    zipfile.write('generator.py')
+    zipfile.write('run.bat')
+    zipdir('tools', zipfile, '.')
+
+logging.info(f"Compiled archive {archive}")
+
