c30ae87e86ccd49651fde6cccb3f3b1c891cdc38
Christian Buehlmann <bue@foryouandyourcustomers.com>
2022-03-16 06:55:42 +0100

    add robustness for missing label coordinates

--- pythonWork/pythonSource/LOAD_MODELS/LOAD_ODM/transferModel.py
+++ pythonWork/pythonSource/LOAD_MODELS/LOAD_ODM/transferModel.py
@@ -684,6 +684,16 @@ def transferdiaconnect(pconnectors, pdiagid, puc, pdc):
                                                                    py=points[len(points) - 1]['y'], pdiagid=pdiagid,
                                                                    pentiid=rela.rela_enti_id_to)
 
+            # HACK around sqlite3.IntegrityError: CHECK constraint failed: relr_stx_chk
+            if relr.relr_starttext_x is None or int(relr.relr_starttext_x) < 0:
+                logging.warning(f"Patching relr_starttext_x({relr.relr_starttext_x}) on {rela.rela_id} to 0")
+                relr.relr_starttext_x = 0
+
+            # HACK around sqlite3.IntegrityError: CHECK constraint failed: relr_sty_chk
+            if relr.relr_starttext_y is None:
+                logging.warning(f"Patching relr_starttext_y({relr.relr_starttext_y}) on {rela.rela_id} to 0")
+                relr.relr_starttext_y = 0
+
             relr.insert()
 
             linesegs = []
